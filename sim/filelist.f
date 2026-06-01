bdc7c43401e1addb0637b3b4f006e6e6f323e69b
Kalfkcrypt <vnguyen@verifasttech.com>
2026-06-01 14:56:56 +0700

    Test write sequence ver 1.1

--- sim/filelist.f
+++ sim/filelist.f
@@ -1,9 +1,6 @@
 # =============================================================================
-# Filelist cho AXI4 SRAM UVM Verification (QuestaSim) - Style include
+# Filelist cho AXI4 SRAM UVM Verification (QuestaSim 10.6b)
 # =============================================================================
-
-# UVM
--uvm
 
 # RTL (DUT)
 ../rtl/m_vlsi_arbiter.sv
@@ -12,7 +9,7 @@
 ../rtl/m_vlsi_sram_misc.sv
 ../rtl/m_vlsi_axi4_sram.sv
 
-# TB - Include directories
+# TB Include directories
 +incdir+../tb/include
 +incdir+../tb/interface
 +incdir+../tb/agent
@@ -21,10 +18,10 @@
 +incdir+../tb/scoreboard
 +incdir+../tb/test
 
-# Package (nằm trong include)
+# Package
 ../tb/include/axi4_pkg.sv
 
-# Các file TB còn lại
+# TB files
 ../tb/interface/axi4_if.sv
 
 ../tb/agent/axi4_sequencer.sv
@@ -40,5 +37,5 @@
 ../tb/scoreboard/axi4_scoreboard.sv
 ../tb/test/base_test.sv
 
-# Top
+# Top module
 ../tb/top/tb_top.sv
