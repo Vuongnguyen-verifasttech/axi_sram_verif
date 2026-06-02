f72f273f026634b6e3c369393a4d55a62de767fb
Kalfkcrypt <vnguyen@verifasttech.com>
2026-06-02 11:44:48 +0700

    Axi4 NEO ver 1.22: Fix syntax error in env_pkg & tb_top

--- tb/tb_top.sv
+++ tb/tb_top.sv
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 import uvm_pkg::*;
-include "uvm_macros.svh"
+`include "uvm_macros.svh"
 
 // =============================================================================
 // tb_top.sv
