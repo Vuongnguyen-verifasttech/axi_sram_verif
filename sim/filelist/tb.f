676abdd2dc1cbb064b51ea43d0512cc5d79ff60d
Kalfkcrypt <vnguyen@verifasttech.com>
2026-06-02 11:22:59 +0700

    Axi4 NEO ver 1.2: Fix syntax error

--- sim/filelist/tb.f
+++ sim/filelist/tb.f
@@ -1,5 +1,5 @@
 # UVM Packages (compile order rất quan trọng)
-../env/axi4_agent/axi4_agent_pkg.sv
+../env/agent/axi4_agent_pkg.sv
 ../env/axi4_env_pkg.sv
 ../test/axi4_test_pkg.sv
 
--- test/axi4_test_pkg.sv
+++ test/axi4_test_pkg.sv
@@ -1,23 +1,16 @@
 `timescale 1ns/1ps
 
-// =============================================================================
-// axi4_test_pkg.sv
-// Package gom tất cả testcase
-// =============================================================================
-
 package axi4_test_pkg;
 
     import uvm_pkg::*;
     `include "uvm_macros.svh"
 
-    // Import environment package
     import axi4_env_pkg::*;
 
-    // Include tất cả test
-    `include "axi4_base_test.sv"
+    // Include sequence trước test
+    `include "../seq/base/axi4_base_seq.sv"
 
-    // Các test khác sẽ include sau
-    // `include "axi4_smoke_test.sv"
-    // `include "axi4_regression_test.sv"
+    // Include test
+    `include "axi4_base_test.sv"
 
 endpackage : axi4_test_pkg
