b36ea41f5a47dfe35d0cdb68ba27a61f201935c5
Kalfkcrypt <vnguyen@verifasttech.com>
2026-06-15 13:59:06 +0700

    Ver 1.53: fix thu tu pkg

--- sim/filelist/tb.f
+++ sim/filelist/tb.f
@@ -1,6 +1,7 @@
 # UVM Packages (compile order rất quan trọng)
-../seq/axi4_vseqr_pkg.sv
 ../env/agent/axi4_agent_pkg.sv
+../seq/axi4_vseqr_pkg.sv // agent pkg must the first 
+
 ../env/axi4_env_pkg.sv
 ../seq/axi4_seq_pkg.sv
 ../test/axi4_test_pkg.sv
