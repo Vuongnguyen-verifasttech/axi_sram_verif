e6d8370b79ed32237e1e963b76a0e887be207628
Kalfkcrypt <vnguyen@verifasttech.com>
2026-06-16 17:01:25 +0700

    Ver 1.72: chay reset tesst

--- seq/axi4_seq_pkg.sv
+++ seq/axi4_seq_pkg.sv
@@ -31,8 +31,9 @@ package axi4_seq_pkg;
     // Specific Read Sequences
     // =====================================================================
     `include "../seq/sequences/axi4_single_read_seq.sv"
-    `include "../seq/base/axi4_base_seq.sv"
+    
     `include "../seq/sequences/axi4_wr_rd_integrity_seq.sv"
+    `include "../seq/base/axi4_base_seq.sv"
    
     `include "../seq/sequences/axi4_reset_seq.sv"
     `include "../seq/sequences/axi4_reset_sanity_seq.sv"
